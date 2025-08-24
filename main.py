a82adc218cf3f15e8abba369a183f63a3e3bdde4
bielico <gkortschinski@gmail.com>
2025-08-24 17:00:55 -0300

    feat: Extração do texto de cada pdf utilizando pdfplumber

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import openpyxl
 from openpyxl.workbook import Workbook
+import pdfplumber
 
 directory = "pdf"
 files = os.listdir(directory)
@@ -23,5 +24,10 @@ last_empty_line = 1
 while ws["A" + str(last_empty_line)].value is not None:
     last_empty_line += 1
 
-
-
+# Para file
+for file in files:
+    # Abrir seu pdf
+    with pdfplumber.open(directory + "/" + file) as pdf:
+        first_page = pdf.pages[0]
+        pdf_text = first_page.extract_text()
+        print(pdf_text)
