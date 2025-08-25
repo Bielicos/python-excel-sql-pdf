6c3639be9aae31dedf6be6abd9f767c90661932c
bielico <gkortschinski@gmail.com>
2025-08-25 17:38:33 -0300

    feat: Conexão com o banco de dados feita

--- main.py
+++ main.py
@@ -4,45 +4,52 @@ from openpyxl.workbook import Workbook
 import pdfplumber
 import re
 from datetime import datetime
+import mysql.connector
 
 def main():
-    try:
-        directory = "pdf"
-        files = os.listdir(directory)
-        files_quantity = len(files)
-    except Exception as error:
-        print(f"\nException: {error}\nResult: Could not create directory with pdfs")
+    # STARTUP
+
+    # Database connection
+    db = mysql.connector.connect(
+        host="localhost",
+        user="user123",
+        password="password123",
+        database="process_invoices"
+    )
+
+    cursor = db.cursor()
+
+    # Get files from directory
+    directory = "pdf"
+    files = os.listdir(directory)
+    files_quantity = len(files)
 
     if files_quantity == 0:
         raise Exception("\nException: No files found in the directory")
 
-    try:
-        wb = Workbook()
-        ws = wb.active
-        ws.title = "Invoice Imports"
+    # Create excel file
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Invoice Imports"
 
-        # Referenciando as colunas do Excel
-        ws['A1'] = "Invoice #"
-        ws['B1'] = "Date"
-        ws['C1'] = "File name"
-        ws['D1'] = "Status"
+    ws['A1'] = "Invoice #"
+    ws['B1'] = "Date"
+    ws['C1'] = "File name"
+    ws['D1'] = "Status"
 
-        last_empty_line = 1
-        while ws["D" + str(last_empty_line)].value is not None:
-            last_empty_line += 1
-    except Exception as error:
-        print(f"\nException: {error}\nResult: Could not create excel file")
+    last_empty_line = 1
+    while ws["D" + str(last_empty_line)].value is not None:
+        last_empty_line += 1
 
-
-    # Para file
+    # WORK
     for file in files:
         try:
-            # Abrir seu pdf
+            # Abrindo o pdf
             with pdfplumber.open(directory + "/" + file) as pdf:
                 first_page = pdf.pages[0]
                 pdf_text = first_page.extract_text()
 
-            # Instrucao Regex
+            # Regex
             inv_number_re_pattern = r"INVOICE #(\d+)"
             inv_date_re_pattern = r"DATE: (\d{2}/\d{2}/\d{4})"
 
@@ -72,13 +79,10 @@ def main():
             ws["D" + str(last_empty_line)] = f"Exception: {error}"
             last_empty_line += 1
 
-    try:
-        full_now = str(datetime.now()).replace(":", "-")
-        dot_index = full_now.index(".")
-        now = full_now[:dot_index]
-        wb.save("Invoices -" + str(now) + ".xlsx")
-    except Exception as error:
-        print(f"\nException: {error}\nResult: Could not save the excel file")
+    full_now = str(datetime.now()).replace(":", "-")
+    dot_index = full_now.index(".")
+    now = full_now[:dot_index]
+    wb.save("Invoices -" + str(now) + ".xlsx")
 
 if __name__ == "__main__":
     main()
